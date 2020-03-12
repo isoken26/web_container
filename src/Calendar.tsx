6de6e730ae6e4e208b840d3306ba37859735e8ac
shinoda <tetora1053@gmail.com>
2020-03-12 22:51:52 +0900

    apollo-serverとapollo-clientをお試し

--- src/Calendar.tsx
+++ src/Calendar.tsx
@@ -1,11 +1,35 @@
 import React from 'react';
 import './Calendar.css';
 import Header from './components/Header'
+import ApolloClient from 'apollo-boost';
+import { gql } from "apollo-boost";
 
 const Calender = () => {
+  const getEvent = () => {
+    const client = new ApolloClient({
+      uri: 'http://gql.tetora1053.jp',
+    })
+
+    client
+      .query({
+        query: gql`
+          {
+            events {
+              name
+            }
+          }
+        `
+      })
+      .then(result => console.log(result));
+      }
+
   return (
     <div className="Calender">
       <Header></Header>
+
+      <div>
+        <button onClick={getEvent}>getEvent</button>
+      </div>
     </div>
   );
 }
